4d0d00a7b050b7ace81d0c2eaa591592d4737e70
reichlj <jakobreichl@gmail.com>
2021-03-14 16:28:37 +0100

    Examples 5.4.x completed

--- K5_ComputerVision/K542VisualizeConvetFilter.py
+++ K5_ComputerVision/K542VisualizeConvetFilter.py
@@ -4,11 +4,14 @@ from keras.applications import VGG16
 from keras import backend as K
 
 def deprocess_image(x):
+    # normalize tensor: center on 0., ensure std is 0.1
     x -= x.mean()
     x /= (x.std() + 1e-5)
     x *= 0.1
+    # clip to [0, 1]
     x += 0.5
     x = np.clip(x, 0, 1)
+    # convert to RGB array
     x *= 255
     x = np.clip(x, 0, 255).astype('uint8')
     return x
@@ -24,21 +27,22 @@ def generate_pattern(layer_name, filter_index, size=150):
     input_img_data = np.random.random((1, size, size, 3)) * 20 + 128.
     step = 1.
     for i in range(40):
+        # Compute the loss value and gradient value
         loss_value, grads_value = iterate([input_img_data])
+        # Here we adjust the input image in the direction that maximizes the loss
         input_img_data += grads_value * step
     img = input_img_data[0]
     return deprocess_image(img)
 
 model = VGG16(weights='imagenet',include_top=False)
 
-layer_name = 'block3_conv1'
-filter_index = 0
+layer_name = 'block4_conv1'
+filter_index = 1
 image1= generate_pattern(layer_name, filter_index)
-plt.imshow(generate_pattern(layer_name, filter_index))
 plt.imshow(image1)
 plt.show()
 
-layer_name = 'block1_conv1'
+layer_name = 'block4_conv1'
 size = 64
 margin = 5
 results = np.zeros((8 * size + 7 * margin, 8 * size + 7 * margin, 3),dtype='uint8')
--- K5_ComputerVision/K543HeatmapOfClassactivation.py
+++ K5_ComputerVision/K543HeatmapOfClassactivation.py
@@ -11,6 +11,7 @@ K.clear_session()
 # Note that we are including the densely-connected classifier on top;
 # all previous times, we were discarding it.
 model = VGG16(weights='imagenet')
+model.summary()
 
 # The local path to our target image
 img_path = r'C:\data\images'
@@ -20,27 +21,22 @@ img_path_elephant = os.path.join(img_path,r'creative_commons_elephant.jpg')
 img = image.load_img(img_path_elephant, target_size=(224, 224))
 # `x` is a float32 Numpy array of shape (224, 224, 3)
 x = image.img_to_array(img)
-# We add a dimension to transform our array into a "batch"
-# of size (1, 224, 224, 3)
+# We add a dimension to transform our array into a "batch" of size (1, 224, 224, 3)
 x = np.expand_dims(x, axis=0)
-# Finally we preprocess the batch
-# (this does channel-wise color normalization)
+# Finally we preprocess the batch - channel-wise color normalization)
 x = preprocess_input(x)
 
 # This is the "african elephant" entry in the prediction vector
 african_elephant_output = model.output[:, 386]
-# The is the output feature map of the `block5_conv3` layer,
-# the last convolutional layer in VGG16
+# output feature map of the `block5_conv3` layer, last convolutional layer in VGG16
 last_conv_layer = model.get_layer('block5_conv3')
-# This is the gradient of the "african elephant" class with regard to
-# the output feature map of `block5_conv3`
+# gradient of "african elephant" class with regard to output feature map of `block5_conv3`
 grads = K.gradients(african_elephant_output, last_conv_layer.output)[0]
 # This is a vector of shape (512,), where each entry
 # is the mean intensity of the gradient over a specific feature map channel
 pooled_grads = K.mean(grads, axis=(0, 1, 2))
 # This function allows us to access the values of the quantities we just defined:
-# `pooled_grads` and the output feature map of `block5_conv3`,
-# given a sample image
+# `pooled_grads` and the output feature map of `block5_conv3`, given a sample image
 iterate = K.function([model.input], [pooled_grads, last_conv_layer.output[0]])
 # These are the values of these two quantities, as Numpy arrays,
 # given our sample image of two elephants
@@ -50,8 +46,7 @@ pooled_grads_value, conv_layer_output_value = iterate([x])
 for i in range(512):
     conv_layer_output_value[:, :, i] *= pooled_grads_value[i]
 
-# The channel-wise mean of the resulting feature map
-# is our heatmap of class activation
+# channel-wise mean of the resulting feature map is our heatmap of class activation
 heatmap = np.mean(conv_layer_output_value, axis=-1)
 
 heatmap = np.maximum(heatmap, 0)
