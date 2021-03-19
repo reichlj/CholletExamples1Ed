a3ae32ebe2781ba7e611683ba2bd13439fd91368
reichlj <jakobreichl@gmail.com>
2021-03-19 22:00:18 +0100

    FunctionalAPI example

---
+++ K7/K711_FunctionalAPI.py
@@ -0,0 +1,20 @@
+from keras.models import Sequential, Model
+from keras import layers
+from keras import Input
+
+seq_model = Sequential()
+seq_model.add(layers.Dense(32, activation='relu', input_shape=(64,)))
+seq_model.add(layers.Dense(32, activation='relu'))
+seq_model.add(layers.Dense(10, activation='softmax'))
+
+input_tensor = Input(shape=(32,))
+dense = layers.Dense(32, activation='relu')
+output_tensor = dense(input_tensor)
+
+input_tensor = Input(shape=(64,))
+x = layers.Dense(32, activation='relu')(input_tensor)
+x = layers.Dense(32, activation='relu')(x)
+output_tensor = layers.Dense(10, activation='softmax')(x)
+model = Model(input_tensor, output_tensor)
+model.summary()
+
